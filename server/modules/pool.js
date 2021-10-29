dc11f578fc7019542a7f1d952a66e49aada94652
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-29 16:59:57 -0500

    pool created

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,22 @@
 const pg = require('pg');
 
+const Pool = pg.Pool;
 
+const pool = new Pool({
+    database: 'prime_feedback',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis:30000
+})
 
-module.exports = new pg.Pool(config);
+pool.on('connect', () => {
+    console.log('Connected to DB')
+})
+
+pool.on('error', () => {
+    console.log('Error connecting to DB');
+    
+})
+
+module.exports = pool;
