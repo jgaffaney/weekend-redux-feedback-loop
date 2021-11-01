ff98706a571c1ec1767edc3102466ec188563a12
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-01 15:29:31 -0500

    pool updated for deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,22 +1,36 @@
 const pg = require('pg');
+const url = require('url');
+let config = {};
 
-const Pool = pg.Pool;
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
 
-const pool = new Pool({
-    database: 'prime_feedback',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis:30000
-})
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: { rejectUnauthorized: false },
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
 
-pool.on('connect', () => {
-    console.log('Connected to DB')
-})
+} else {
+    // only change the things on the right side of the ||
+    config = {
+        user: process.env.PG_USER || null, //env var: PGUSER
+        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+        database: process.env.DATABASE_NAME || 'prime_feedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
 
-pool.on('error', () => {
-    console.log('Error connecting to DB');
-    
-})
+module.exports = new pg.Pool(config);// const pg = require('pg');
 
-module.exports = pool;
